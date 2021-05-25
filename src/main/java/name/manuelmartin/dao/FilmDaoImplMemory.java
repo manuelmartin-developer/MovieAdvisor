df4f1d50511a426077e1cf7bc4c2993692b68fc0
ManuelMartin <hola@manuelmartin.name>
2021-05-25 09:29:29 +0200

    Finished DAO

--- src/main/java/name/manuelmartin/dao/FilmDaoImplMemory.java
+++ src/main/java/name/manuelmartin/dao/FilmDaoImplMemory.java
@@ -4,9 +4,12 @@ package name.manuelmartin.dao;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import name.manuelmartin.movieadvisor.config.AppConfig;
 import name.manuelmartin.movieadvisor.model.Film;
 
 @Repository
@@ -14,35 +17,66 @@ public class FilmDaoImplMemory implements FilmDao {
 	
 	public List<Film> peliculas = new ArrayList<>();
 	
+	@Autowired
+	private AppConfig appConfig;
+	
 	public void init() {
 		
-		peliculas = UtilFilmFileReader.readFile(path, separator, listseparator);
+		peliculas = UtilFilmFileReader.readFile(appConfig.getFile(), appConfig.getSeparator(), appConfig.getListSeparator());
 		
 	}
 
 	public Film findById(long id) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		// @formatter:off
+ 
+			Optional<Film> result = peliculas
+					.stream()
+					.filter(f -> f.getId() == id)
+					.findFirst();
+		
+		// @formatter:on
+			
+			return result.orElse(null);
+
 	}
 
 	public Collection<Film> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return peliculas;
 	}
 
 	public void insert(Film film) {
-		// TODO Auto-generated method stub
+		peliculas.add(film);
 
 	}
 
 	public void edit(Film film) {
-		// TODO Auto-generated method stub
+		int index = getIndexOf(film.getId());
+		if(index != -1)
+			peliculas.set(index, film);
 
 	}
 
-	public void delete(Film film) {
-		// TODO Auto-generated method stub
+	public void delete(Long id) {
+		int index = getIndexOf(id);
+		if(index != -1)
+			peliculas.remove(index);
 
+	}
+	
+	private int getIndexOf(long id) {
+		boolean encontrado = false;
+		int index = 0;
+		
+		while (!encontrado && index < peliculas.size()) {
+			if(peliculas.get(index).getId() == id)
+				encontrado = true;
+			else
+				index++;
+		}
+		
+		return (encontrado) ? index : -1;
 	}
 
 }
--- src/main/java/name/manuelmartin/movieadvisor/config/AppConfig.java
+++ src/main/java/name/manuelmartin/movieadvisor/config/AppConfig.java
@@ -1,10 +1,52 @@
 package name.manuelmartin.movieadvisor.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = "name.manuelmartin.movieadvisor")
+@PropertySource("classpath:movieadvisor.properties")
 public class AppConfig {
+
+	@Value("${file.path}")
+	private String file;
+	
+	@Value("${file.csv.separator}")
+	private String separator;
+	
+	@Value("${file.csv.list.separator}")
+	private String listSeparator;
+
+	
+	
+	
+	public String getFile() {
+		return file;
+	}
+
+	public void setFile(String file) {
+		this.file = file;
+	}
+
+	public String getSeparator() {
+		return separator;
+	}
+
+	public void setSeparator(String separator) {
+		this.separator = separator;
+	}
+
+	public String getListSeparator() {
+		return listSeparator;
+	}
+
+	public void setListSeparator(String listSeparator) {
+		this.listSeparator = listSeparator;
+	}
+	
+	
+	
 
 }
